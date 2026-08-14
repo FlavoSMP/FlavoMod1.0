a5fb28e804e049831db89fbb8c3ddc7b23758a26
Flavo SMP <ArcDepths@hotmail.com>
2026-08-14 15:21:01 +0200

    Add Logger and SidebarManager initialization

--- src/main/java/com/flavomod/FlavoMod.java
+++ src/main/java/com/flavomod/FlavoMod.java
@@ -1,20 +1,29 @@
 package com.flavomod;
 
-import com.flavomod.commands.SidebarManager; // <--- ADD THIS IMPORT LINE
+import com.flavomod.commands.SidebarManager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FlavoMod implements ModInitializer {
+    public static final String MOD_ID = "flavomod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        LOGGER.info("FlavoMod 1.21.1 initialized!");
+
+        // Initialize sidebar when the server starts
         ServerLifecycleEvents.SERVER_STARTED.register(SidebarManager::setupSidebar);
 
+        // Refresh count on player join
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
             SidebarManager.updateSidebar(server);
         });
 
+        // Refresh count on player leave
         ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
             server.execute(() -> SidebarManager.updateSidebar(server));
         });
