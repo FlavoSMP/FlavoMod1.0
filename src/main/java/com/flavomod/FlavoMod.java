1502438f6e26ae948c9a3679e848a429a2a5e5f0
Flavo SMP <ArcDepths@hotmail.com>
2026-08-14 17:23:02 +0200

    Refactor comments and remove Texturepack registration

--- src/main/java/com/flavomod/FlavoMod.java
+++ src/main/java/com/flavomod/FlavoMod.java
@@ -1,7 +1,6 @@
 package com.flavomod;
 
 import com.flavomod.commands.SidebarManager;
-import com.flavomod.commands.Texturepack;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
@@ -16,18 +15,15 @@ public class FlavoMod implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("FlavoMod 1.21.1 initialized!");
 
-        // 1. Register Texture Pack / Resource Pack enforcement logic
-        Texturepack.register();
-
-        // 2. Initialize sidebar when the server starts
+        // 1. Initialize sidebar when the server starts
         ServerLifecycleEvents.SERVER_STARTED.register(SidebarManager::setupSidebar);
 
-        // 3. Refresh sidebar count on player join
+        // 2. Refresh sidebar count on player join
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
             SidebarManager.updateSidebar(server);
         });
 
-        // 4. Refresh sidebar count on player leave
+        // 3. Refresh sidebar count on player leave
         ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
             server.execute(() -> SidebarManager.updateSidebar(server));
         });
