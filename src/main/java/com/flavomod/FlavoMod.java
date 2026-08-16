eb9fed4a53d0be1adaa3e0324245d6d17545cdf6
Flavo SMP <ArcDepths@hotmail.com>
2026-08-16 23:52:17 +0200

    Remove calls to missing currency lifecycle methods

--- src/main/java/com/flavomod/FlavoMod.java
+++ src/main/java/com/flavomod/FlavoMod.java
@@ -1,10 +1,8 @@
 package com.flavomod;
 
 import com.flavomod.commands.SidebarManager;
-import com.flavomod.commands.Currency;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,21 +15,8 @@ public class FlavoMod implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("FlavoMod 1.21.1 initialized!");
 
-        // Initialize currency system
-        Currency.initialize();
-
-        // Sidebar updates / animation
+        // Currency data is loaded and saved automatically by SavedData.
+        // Sidebar updates also access it when players are online.
         ServerTickEvents.END_SERVER_TICK.register(SidebarManager::tick);
-
-        // Load player currency when they join
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            Currency.loadPlayer(handler.getPlayer());
-        });
-
-        // Save player currency and clean up sidebar when they leave
-        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
-            Currency.savePlayer(handler.getPlayer());
-            SidebarManager.onPlayerLeave(server, handler.getPlayer());
-        });
     }
 }
