b13a62630c643ade37431b02be11354183feda26
Flavo SMP <ArcDepths@hotmail.com>
2026-08-17 19:34:57 +0200

    Fix: Remove invalid ServerPlayerEntity import from Currency.java
    - Removed: import net.minecraft.server.network.ServerPlayerEntity;
    - This class does not exist in the current Minecraft/Fabric version and was causing a compile error.
    - The import was unused in the code anyway.

--- src/main/java/com/flavomod/commands/Currency.java
+++ src/main/java/com/flavomod/commands/Currency.java
@@ -1,6 +1,5 @@
 package com.flavomod.commands;
 
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.nbt.CompoundTag;
